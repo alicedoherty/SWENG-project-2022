595e8922eb225989a1028fbdc716960723fabc10
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2022-02-23 10:31:46 +0000

    small config change

--- truffle-config.js
+++ truffle-config.js
@@ -4,24 +4,24 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  // networks: {
-  //   develop: {
-  //     port: 7545
-  //   }
-  // }
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
-  },
-  compilers: {
-    solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
+    develop: {
+      port: 7545
     }
   }
+  // networks: {
+  //   development: {
+  //     host: "127.0.0.1",
+  //     port: 7545,
+  //     network_id: "*" // Match any network id
+  //   }
+  // },
+  // compilers: {
+  //   solc: {
+  //     optimizer: {
+  //       enabled: true,
+  //       runs: 200
+  //     }
+  //   }
+  // }
 };
