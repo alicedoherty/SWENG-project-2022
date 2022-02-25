e2d2a29f9e56fe9e8015917a9fb9c8ce239b092a
Arynel <54998052+Arynel@users.noreply.github.com>
2022-02-25 15:35:26 +0000

    header, page structure, dependencies in package
    Added header component.
    Added header to create and home pages.
    If you want to access boilerplate app, click the red test button (mine stopped working again).
    Need a couple of dependencies from MUI but I committed the package JSON so running "npm install" in the client folder will take care of installing them.

---
+++ client/src/Create.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '@mui/material/Container'
+import Header from './Header'
+export default function Create (){
+
+
+
+    return(
+        <Container maxWidth="xs">
+          <Header/>
+          
+        </Container>
+    );
+}
