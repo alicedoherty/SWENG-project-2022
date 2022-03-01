395243c8d02ab778c815bcbceb2733d4ba88d5f3
Anastasiya Bogoslovskaya <bogosloa@tcd.ie>
2022-03-01 19:44:47 +0000

    added code code to main

---
+++ client/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Link from '@mui/material/Link'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import IconButton from '@mui/material/IconButton'
+
+
+export default function Header(){
+
+
+}
