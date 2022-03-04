c28eff00fa106d6649c5a7a7341751032e670682
Alex <71922720+alexandersep@users.noreply.github.com>
2022-03-04 12:40:55 +0000

    removed string[] feature + added postTitle
    removed terrible implementation of string[] and added a postTitle variable in User struct, capabilities, is reading postTitle, renaming postTitle and when creating user postTitle is the 3rd parameter that's needed to be specified. This commit will solive 1/3 features needed for the 2nd sprint

--- contracts/PublishContent.sol
+++ contracts/PublishContent.sol
@@ -2,33 +2,30 @@
 pragma solidity >=0.4.21 <8.10.0;
 
 contract PublishContent {
+
     struct User {
         uint id; // identification used for quick searching\
         string name; // e.g. author's name
-        string[] text; // e.g. blogpost, all posts by this particular user
-        uint postAmount; // num of posts user has
+        string text; // e.g. blogpost
+        string postTitle;
     }
-    string[] public allTexts; // will start at index 0, this array will contain all posts that exist 
+
     User[] public users;
     uint public nextId = 1; // so many issues with nextId being 0, therefore starting index is 1
 
-    function create(string memory _name, string memory _text) public {
-        allTexts.push(_text);
-        users.push(User(nextId, _name, allTexts, 1));
+    function create(string memory _name, string memory _text, string memory _postTitle) public {
+        users.push(User(nextId, _name, _text, _postTitle));
         nextId++;
     }
 
     // change text e.g. blogpost (modify text)
     // if user does not exist, it will give userError() but cause error, needs fixing
     function publish(uint _id, string memory _text) public mustBeUser(_id) {
-        allTexts.push(_text);
-        users[_id-1].text = allTexts;
-        users[_id-1].postAmount++;
+        users[_id-1].text = _text;
     }
 
-    // basically deletes any post specified in postNum by setting the string to "" the empty string
-    function removePublish(uint _id, uint postNum) public mustBeUser(_id) { 
-        delete users[_id-1].text[postNum];
+    function renamePostTitle(uint _id, string memory _postTitle) public mustBeUser(_id) {
+        users[_id-1].postTitle = _postTitle;
     }
 
     function readName(uint _id) view public returns(string memory) {
@@ -37,14 +34,17 @@ contract PublishContent {
         userError();
     }
 
-    function readtext(uint _id, uint postNum) view public returns(string memory) { // post 0 -> infinity
+    function readtext(uint _id) view public returns(string memory) {
         require(_id != 0, "User does not exist"); // instead of require, so there aren't warnings
-        if (searchUser(_id)) {
-            return(users[_id-1].text[postNum]); // _id - 1 is actually necessary...  
-        }
+        if (searchUser(_id)) return(users[_id-1].text); // _id - 1 is actually necessary...
         userError();
     }
 
+    function readPost(uint _id) view public returns(string memory) {
+        require(_id != 0, "User does not exist"); // instead of require, so there aren't warnings
+        if (searchUser(_id)) return(users[_id-1].postTitle); // _id - 1 is actually necessary...
+        userError();
+    }
     // be very careful when deleting users, because even with mustBeUser, it will cost money, just how solidity works
     function del(uint _id) public mustBeUser(_id) { // del user, will del the user and make the index unusable, *BAD IMPLEMENTATION*
         delete users[_id-1];
@@ -65,10 +65,10 @@ contract PublishContent {
     }
 
     function isStrEmpty(uint _id) view internal returns(bool) {
-        return keccak256(abi.encodePacked(users[_id].name)) == keccak256(abi.encodePacked(""));
+        return keccak256(abi.encodePacked(users[_id-1].name)) == keccak256(abi.encodePacked(""));
     }
 
-    // since pure it will not be on blockchain pure -> do not read or modify the state variables
+	// since pure it will not be on blockchain pure -> do not read or modify the state variables
     function userError() pure internal {
         revert("User does not exist");
     }
