d030435aa5beaa88c1fe66ab9eb9d7e6c44d855f
Steven Cataluna <cataluns@tcd.ie>
2022-03-14 21:19:58 +0000

    Fixed component proportions and changed colours

--- client/src/Post.js
+++ client/src/Post.js
@@ -7,9 +7,17 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import Stack from '@mui/material/Stack'
+import PropTypes from 'prop-types'
+import { useParams } from 'react-router';
+import post_data from "./data/post_data";
 
 
-export default function Post() {
+function Post(props) {
+    // let {
+    //     post
+    // } = props;
+    const {id} = useParams();
+    const post = post_data[id];
 
     //function createButton()
     
@@ -18,28 +26,17 @@ export default function Post() {
     const defaultString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const testingString = "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway. Because bees don’t care what humans think is impossible."
 
-
     const changeButtonStyle = { // button styles for buttons under the text box
         position: "relative",
         fontSize:18,
         border:1,
-        borderColor:"black",
-        backgroundColor:"#e6f0f7",
+        borderColor:"rgba(28, 54, 100, .6)",
+        color:"#f37026",
         
         '&:hover':{
           //backgroundColor:"#e6f0f7"
         }
       }
-
-    const textButtonStyle = {
-        position: "relative",
-        fontSize: 14,
-
-        '&:hover':{
-            backgroundColor:"cfe8fc"
-        }
-    }
-    
     
     //hook to change the text inside box
     const [currentTitle, setTitle] = useState("Test")
@@ -51,27 +48,26 @@ export default function Post() {
           <Header/>
         </Container>
 
-
         <Container maxWidth="100%" height="100%"> 
 
         <Box sx={{ //box for buttons
             borderRadius: '4px',
             boxShadow:1,
             border: 1, 
-            borderColor: "rgba(111, 172, 252, .4)",
+            borderColor: "#1c3664",
             position: 'absolute',
             left:"75%", top:"10vh",
             width:"15%", height:"60vh",
-            bgcolor: '#e0f1ff'}} >
+            bgcolor: '#f0f6ff'}} >
 
-                <Typography variant = "h5" align = "center" sx={{position:'relative', top:"1%", padding:1.5}}>
+                <Typography variant = "h5" align = "center" sx={{position:'relative', top:"1%", padding:2, color:"#1c3664"}}>
                     Previous versions
                 </Typography>
 
                 <Container>
                 <Stack spacing={4}>
-                    <Button color="primary" variant="outlined" sx={changeButtonStyle} onClick={() => setText(testingString)}>
-                        Change text 1
+                    <Button variant="outlined" sx={changeButtonStyle} onClick={() => setText(testingString)}>
+                        Change text
                     </Button>
                     <Button sx={changeButtonStyle} onClick={() => setText(defaultString)}>
                         Revert Text
@@ -85,58 +81,58 @@ export default function Post() {
             borderRadius: '4px',
             boxShadow:1,
             border: 1, 
-            borderColor: "rgba(111, 172, 252, .4)",
+            borderColor: "#1c3664",
             position: 'relative',
             left: "5%",
             top: "10vh",
             width:"60%", height:"80vh",
-            bgcolor: '#e0f1ff'}} >
+            bgcolor: '#f0f6ff'}} >
 
             
             <Box m="auto" sx={{ //inner box where the text box goes
                 padding: 2.5,
-                borderRadius: '1px',
+                borderRadius: '2px',
                 boxShadow:1,
                 border: 1, 
-                borderColor: "rgba(0,0,0,.5)",
+                borderColor: "rgba(28, 54, 100, .4)",
                 position: 'relative',
                 //left: 40,
                 top: "4%",
                 width:"87.5%", height:"75%",
-                bgcolor: '#e6f0f7'}}>
+                bgcolor: '#f0f6ff'}}>
 
 
                 <Box sx ={{ //box for the title
                     position: "relative",
                     width:"100%", height:"10%",
-                    bgcolor:"#e6f0f7",
-                    display:"flex",
                     borderBottom:1}}>
                         
-                <AccountCircleIcon sx={{position:'relative', left:"95%", top:"5%"}}/>
-
-
-                <Typography variant = "h4">
-                    {currentTitle}
-                </Typography>
-
-                <Typography variant= "h5" sx={{position:'relative', left:"75%"}}>
-                    {userName}
-                </Typography>
-
-                
-
-                
-                
+                    
+                    <Stack // stack for aligning title, author and user icon 
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    spacing={2}>
+                        <Typography variant = "h4" sx={{color:"#1c3664"}}>
+                        {/* {currentTitle} */}
+                        {post.post_title}
+                        </Typography>
+                        <Typography variant= "h5" sx={{align:"justify", color:"#1c3664"}}>
+                            {/* {userName} */}
+                            {post.author}
+                            <AccountCircleIcon sx={{marginLeft:2}}/>
+                        </Typography>
+                    </Stack>
+                   
                 </Box>
 
                 <Box sx ={{ // actual box for text
                     position: "relative",
                     top:"2%",
                     width:"100%", height:"80%",
-                    bgcolor:"#e6f0f7",}}>
+                    }}>
 
-                <Typography variant = "body1">
+                <Typography variant = "body1" sx ={{color:"#1c3664"}}>
                     {currentText}
                 </Typography>
                 </Box>
@@ -144,7 +140,7 @@ export default function Post() {
             </Box>
 
 
-            <Stack
+            <Stack // aligning buttons below text
             position="relative"
             top="5%" 
             direction="row"
@@ -152,19 +148,18 @@ export default function Post() {
             alignItems="flex-end"
             spacing={4}>
                 
-                <Button sx={changeButtonStyle}>
+                <Button sx={changeButtonStyle} size="small">
                     Add note
                 </Button>
 
-                <Button sx={changeButtonStyle}>
+                <Button sx={changeButtonStyle} size="small">
                     Delete
                 </Button>
 
-                <Button sx={changeButtonStyle}>
+                <Button sx={changeButtonStyle} size="small">
                     Change
                 </Button>
             
-
             </Stack>
 
         </Box>
@@ -173,8 +168,10 @@ export default function Post() {
         </div>
 
     );
-
-
-
-
 }
+
+// Post.propTypes = {
+//     post: PropTypes.object.isRequired
+// };
+
+export default Post;
