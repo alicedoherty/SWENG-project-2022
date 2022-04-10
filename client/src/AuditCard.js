2ca460b40b475380afc2a674cc6d9fde7c4ef3ed
Alice Doherty <66687032+alicedoherty@users.noreply.github.com>
2022-04-10 21:52:57 +0100

    cleaning up code

--- client/src/AuditCard.js
+++ client/src/AuditCard.js
@@ -5,31 +5,28 @@ import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 
 function AuditCard(props) {
+    let { post } = props;
 
-    let{post} = props;
-
-
-    return(
-        <Card variant = "outlined"
-        sx={{
-            height:"8%",
-            bgcolor:"#e6efff"
-        }}
+    return (
+        <Card variant="outlined"
+            sx={{
+                height: "8%",
+                bgcolor: "#e6efff"
+            }}
         >
-            <CardContent sx={{display:"flex"}}>
-                
-                    <Typography variant = "h6" sx={{ color: "#1c3664", fontWeight: "normal" }}>
-                        {post.post_version_id+1}
-                    </Typography>
-                    <Typography variant = "h6" sx={{ color: "#1c3664", position:"absolute", left:"18%", fontWeight: "normal" }}>
-                        {post.date}
-                    </Typography>
-                    <Typography variant = "h6" sx={{ color: "#1c3664", position:"absolute", left:"36%", fontWeight: "normal"  }}>
-                        {post.author}
-                    </Typography>
-                    <Typography variant = "h6" sx={{ color: "#1c3664", position:"absolute", left:"60%", fontWeight: "normal"  }}>
-                        {post.post_title}
-                    </Typography>
+            <CardContent sx={{ display: "flex" }}>
+                <Typography variant="h6" sx={{ color: "#1c3664", fontWeight: "normal" }}>
+                    {post.post_version_id + 1}
+                </Typography>
+                <Typography variant="h6" sx={{ color: "#1c3664", position: "absolute", left: "18%", fontWeight: "normal" }}>
+                    {post.date}
+                </Typography>
+                <Typography variant="h6" sx={{ color: "#1c3664", position: "absolute", left: "36%", fontWeight: "normal" }}>
+                    {post.author}
+                </Typography>
+                <Typography variant="h6" sx={{ color: "#1c3664", position: "absolute", left: "60%", fontWeight: "normal" }}>
+                    {post.post_title}
+                </Typography>
             </CardContent>
         </Card>
     );
@@ -37,6 +34,6 @@ function AuditCard(props) {
 
 AuditCard.propTypes = {
     post: PropTypes.object.isRequired
-  };
+};
 
 export default AuditCard;
--- client/src/Header.js
+++ client/src/Header.js
@@ -9,64 +9,56 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import IconButton from '@mui/material/IconButton'
 import Box from '@mui/material/Box'
 
-export default function Header(){
+export default function Header() {
 
-    const buttonLinkStyle = {
-      color: "#1c3564",
-      fontSize:14,
-      '&:hover':{
-        color:"#f37026",
-      },
-    }
+  const buttonLinkStyle = {
+    color: "#1c3564",
+    fontSize: 14,
+    '&:hover': {
+      color: "#f37026",
+    },
+  }
 
-    const buttonStyle = {
-      '&:hover':{
-        boxShadow:"none",
-        backgroundColor: "transparent",
-      },
-    }
-     
+  const buttonStyle = {
+    '&:hover': {
+      boxShadow: "none",
+      backgroundColor: "transparent",
+    },
+  }
 
-    return (
-        <AppBar sx={{bgcolor:"white"}} position="fixed" elevation={0}>
-          <Toolbar>
-            <Box
-              component="img"
-              sx={{
-                width: 250,
-              }}
-              alt="Propylon Logo"
-              src="../PropylonLogo.png"
-            />
-            <Container sx={{flexGrow:1}} maxWidth="md" >
-              <Button sx={buttonStyle}>
-                <Link underline="none" href="/" sx={buttonLinkStyle}>
-                  Home
-                </Link>
-              </Button>
-              <Button sx={buttonStyle}> 
-                <Link underline="none" href="/search" sx={buttonLinkStyle}>
-                  Search
-                </Link>
-              </Button>  
-              <Button sx={buttonStyle} >
-                <Link underline="none" href="/create" sx={buttonLinkStyle}>
-                  Create Post
-                </Link>
-              </Button>
-              {/* <Button sx={{backgroundColor:"red", "&:hover":{backgroundColor:"gray"}}}>
-                <Link underline="none" href="/test" sx={buttonLinkStyle} >
-                  TEST!!
-                </Link>
-              </Button>  */}
-            </Container>
-            <Typography variant="h7" sx={{color:"#1c3564"}} > 
-                Welcome User
-            </Typography>
-            <IconButton aria-label="user"  sx={{marginLeft:2}}>
-              <AccountCircleIcon/>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar sx={{ bgcolor: "white" }} position="fixed" elevation={0}>
+      <Toolbar>
+        <Box
+          component="img"
+          sx={{
+            width: 250,
+          }}
+          alt="Propylon Logo"
+          src="../PropylonLogo.png"
+        />
+
+        <Container sx={{ flexGrow: 1 }} maxWidth="md" >
+          <Button sx={buttonStyle}>
+            <Link underline="none" href="/" sx={buttonLinkStyle}>
+              Home
+            </Link>
+          </Button>
+
+          <Button sx={buttonStyle}>
+            <Link underline="none" href="/search" sx={buttonLinkStyle}>
+              Search
+            </Link>
+          </Button>
+
+          <Button sx={buttonStyle} >
+            <Link underline="none" href="/create" sx={buttonLinkStyle}>
+              Create Post
+            </Link>
+          </Button>
+          
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
 }
